272109ef5f9821cd46579bd33a6aedc175b3c277
Giuseppe Vassallo <giuseppe.v1999@gmail.com>
2024-02-08 13:14:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import Footer from "./components/footer/Footer";
-import NotFound from "./components/notFound/Notfound";
+import NotFound from "./components/notFound/NotFound";
 import SearchResult from "./components/searchResult/SearchResult";
 import Recipe from "./components/recipeInfo/RecipeInfo";
 import { Route, Routes } from "react-router-dom";
