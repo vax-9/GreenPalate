288f6c21c3db9276b59499a6feb6f06876eea195
Giuseppe Vassallo <giuseppe.v1999@gmail.com>
2024-02-19 09:11:48 +0100

    v1.4.1

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,24 @@
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
-
+import { logo } from "./assets/images";
 import TrendingRecipe from "./components/trendingRecipe/TrendingRecipe";
 import NavScroll from "./components/nav/NavScroll";
+import { Helmet } from "react-helmet-async";
 
 function Home() {
   return (
     <>
+      <Helmet>
+        <title>GreenPalate</title>
+        <meta name="description" content="the best vegetarian recipes!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://greenpalate.netlify.app/" />
+        <meta
+          property="og:title"
+          content="GreenPalate | the best vegeterian recipes"
+        />
+        <meta property="og:image" content={logo} />
+      </Helmet>
       <NavScroll />
       <Hero />
       <About />
