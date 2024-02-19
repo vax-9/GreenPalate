70d0e99b56d8c78ac42ebfadddf78b53087ef865
Giuseppe Vassallo <giuseppe.v1999@gmail.com>
2024-02-19 09:17:31 +0100

    v1.4.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ import SearchResult from "./pages/searchResult/SearchResult";
 import RecipeInfo from "./pages/recipeInfo/RecipeInfo";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import { Helmet } from "react-helmet-async";
+import { logo } from "./assets/images";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>GreenPalate</title>
+        <meta name="description" content="the best vegetarian recipes!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://greenpalate.netlify.app/" />
+        <meta
+          property="og:title"
+          content="GreenPalate | the best vegeterian recipes"
+        />
+        <meta property="og:image" content={logo} />
+      </Helmet>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
